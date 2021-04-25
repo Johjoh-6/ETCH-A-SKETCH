61ce2b8083fed5db0b7e646d51b17406ac36036f
Johjoh-6 <six.johann@gmail.com>
2021-04-24 20:30:23 -0400

    Rmove a console.log. Change a clear function some of the action was useless

--- main.js
+++ main.js
@@ -13,8 +13,6 @@ clearGridButton();
 // set a grid size
 function getSizeGrid(size) {
     let gridSize = size * size;
-    console.log(size);
-    console.log(gridSize);
     document.getElementById('container').style.gridTemplateRows = `repeat(${size}, 1fr)`;
     document.getElementById('container').style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     for (i = 0; i < gridSize; i++) {
@@ -28,10 +26,9 @@ function getSizeGrid(size) {
 function setColor(a) {
     // this const set a number between 50 - 150
     const randomColor = Math.floor(Math.random() * 100 + 50); 
-    console.log(randomColor);
     a.target.style.backgroundColor = `rgb(${randomColor}, ${randomColor}, ${randomColor})`;
 }
-// Create a button for clear
+// Create a button for clear grid
 function clearGridButton() {
     const buttonClear = document.createElement('button');
     buttonClear.id = 'clear-button';
@@ -41,24 +38,17 @@ function clearGridButton() {
     
 }
 function clearGrid() {
-    let gridColor = container.querySelectorAll('div');
-    gridColor.forEach(grid => grid.style.backgroundColor = '#fff');
-    let reset = Array.from(container.childNodes);;
-        reset.forEach((element) => {
-           container.removeChild(element);
-        })
+    container.querySelectorAll('.grid-element').forEach(elem => elem.remove())
     }
 
 // Create a input area for select the size of the grid
 function setSize() {
-    console.log('set');
     let size = parseInt(prompt('Select the size of the grid'));
     if ( size < 1 || size > 64 || Number.isNaN(size)) {
         alert("Enter a number betwenn 1 and 64");
         setSize();
     } else {
         clearGrid();
-        getSizeGrid(size);
-    
+        getSizeGrid(size); 
 }
 }
